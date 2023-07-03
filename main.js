e0e1f6270d7bf3cdf46b73cd7379d381d2969811
Ayush Kumar <94097145+Itsayu@users.noreply.github.com>
2023-07-03 15:38:11 +0530

    Update main.js

--- main.js
+++ main.js
@@ -12,11 +12,14 @@ function showContent(component) {
     
     if (component === 'home') {
         contentElement.innerHTML = '<h1>Home</h1><p>Welcome to the home page!</p>';
-    } else if (component === 'contact') {
+    } 
+    else if (component === 'contact') {
         contentElement.innerHTML = '<h1>Contact</h1><p>Contact information goes here.</p>';
-    } else if (component === 'information') {
+    } 
+    else if (component === 'information') {
         contentElement.innerHTML = '<h1>Information</h1><p>Additional information is displayed here.</p>';
-    } else if (component === 'guide') {
+    } 
+    else if (component === 'guide') {
         contentElement.innerHTML = '<h1>Guide</h1><p>Helpful guide and instructions are shown in this section.</p>';
     }
 }
